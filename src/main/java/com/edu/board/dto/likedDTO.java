3cd77f543dfdd6a4505d68eae9555ba186486a11
dabin <dabinkim40@gmail.com>
2023-08-23 12:21:40 +0900

    1222

---
+++ src/main/java/com/edu/board/dto/likedDTO.java
@@ -0,0 +1,27 @@
+package com.edu.board.dto;
+
+public class likedDTO {
+	
+	private String id;
+	private boolean jjim;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public boolean isJjim() {
+		return jjim;
+	}
+	public void setJjim(boolean jjim) {
+		this.jjim = jjim;
+	}
+	
+	@Override
+	public String toString() {
+		return "likedDTO [id=" + id + ", jjim=" + jjim + "]";
+	}
+	
+}
