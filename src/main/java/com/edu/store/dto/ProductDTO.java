e2de16c28e5b55f3ee8baf497c9fe631cb391dce
DB <emfprhs1579@gmail.com>
2023-08-23 22:12:56 +0900

    first-1

--- src/main/java/com/edu/store/dto/ProductDTO.java
+++ src/main/java/com/edu/store/dto/ProductDTO.java
@@ -5,7 +5,6 @@ public class ProductDTO {
 	private String product_id;
 	private String display_product_id;
 	private int category_code;
-	private int product_stock;			// 재고
 	public String getProduct_id() {
 		return product_id;
 	}
@@ -24,15 +23,9 @@ public class ProductDTO {
 	public void setCategory_code(int category_code) {
 		this.category_code = category_code;
 	}
-	public int getProduct_stock() {
-		return product_stock;
-	}
-	public void setProduct_stock(int product_stock) {
-		this.product_stock = product_stock;
-	}
 	@Override
 	public String toString() {
 		return "ProductDTO [product_id=" + product_id + ", display_product_id=" + display_product_id
-				+ ", category_code=" + category_code + ", product_stock=" + product_stock + "]";
+				+ ", category_code=" + category_code + "]";
 	}
 }
