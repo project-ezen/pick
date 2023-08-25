12a7458a7769c27a8b014767580320a609561818
Lylis99 <133727840+Lylis99@users.noreply.github.com>
2023-08-25 19:57:25 +0900

    SoleDAOImpl.java

--- src/main/java/com/edu/sole/dao/SoleDAOImpl.java
+++ src/main/java/com/edu/sole/dao/SoleDAOImpl.java
@@ -9,25 +9,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
-import com.edu.sole.soledto.RecipedSoleDTO;
-import com.edu.sole.soledto.SoleSearchCriteria;
-import com.edu.sole.soledto.liveSoleDTO;
-import com.edu.sole.soledto.recipe.recipeDTO;
+import com.edu.sole.dto.RecipedSoleDTO;
+import com.edu.sole.dto.SoleSearchCriteria;
+import com.edu.sole.dto.LiveSoleDTO;
+import com.edu.sole.dto.recipe.RecipeDTO;
 
 @Repository
-public class soleDAOImpl implements soleDAO {
+public class SoleDAOImpl implements SoleDAO {
 
-	private static final Logger logger = LoggerFactory.getLogger(soleDAOImpl.class);
-	
+	private static final Logger logger = LoggerFactory.getLogger(SoleDAOImpl.class);
+
 	@Inject
 	private SqlSession sqlSession;
-	
+
 	private static final String namespace = "com.edu.sole.mappers.soleMapper";
-	
+
 	@Override
-	public List<liveSoleDTO> soleMain(SoleSearchCriteria solesearchcriteria) {
+	public List<LiveSoleDTO> soleMain(SoleSearchCriteria solesearchcriteria) {
 		// TODO Auto-generated method stub
-		
+
 		logger.info("DAO입장");
 		logger.info("술 메인의 solesearchcriteria" + solesearchcriteria);
 		return sqlSession.selectList(namespace + ".soleMain", solesearchcriteria);
@@ -36,24 +36,24 @@ public class soleDAOImpl implements soleDAO {
 
 	@Override
 	public int solecount(SoleSearchCriteria solesearchcriteria) {
-		
+
 		logger.info("술 카운트의 solesearchcriteria" + solesearchcriteria);
 		return sqlSession.selectOne(namespace + ".solecount", solesearchcriteria);
 	}
-	
-	
+
+
 	////////////////////////// 술 디테일 부분 ///////////////////////////////////////////////////////////////////////////
 	//술 레시피 구하는
 	@Override
-	public List<recipeDTO> solerecipeDetail(String recipe_code) throws Exception {
-		
+	public List<RecipeDTO> solerecipeDetail(String recipe_code) throws Exception {
+
 		return sqlSession.selectList(namespace + ".solerecipeDetail", recipe_code);
 	}
-	
+
 	// 술 정보 구하는
 	@Override
 	public RecipedSoleDTO solerecipe(String recipe_code) throws Exception {
-		
+
 		return sqlSession.selectOne(namespace + ".solerecipe", recipe_code);
 	}
 
