6bf58de7f3d128fd89101c930bd3db7589a0f49a
SeungHyun Seo <sah696@naver.com>
2023-08-25 10:09:58 +0900

    commit

--- src/main/java/com/edu/sole/service/SoleService.java
+++ src/main/java/com/edu/sole/service/SoleService.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import com.edu.sole.DAO.soleDAO;
+import com.edu.sole.DAO.SoleDAO;
 import com.edu.sole.DTO.SoleSearchCriteria;
 import com.edu.sole.DTO.liveSoleDTO;
 
-public interface soleService {
+public interface SoleService {
 	
 	// 술 목록 보여주는
 	public List<liveSoleDTO> soleMain(SoleSearchCriteria solesearchcriteria);
