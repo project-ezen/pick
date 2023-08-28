ca2dddd8d208e2d80640b50e041175406c7d9cc8
SeungHyun Seo <sah696@naver.com>
2023-08-28 12:04:05 +0900

    1204

--- src/main/java/com/edu/shopping/dto/OrderDTO.java
+++ src/main/java/com/edu/shopping/dto/OrderDTO.java
@@ -2,14 +2,15 @@ package com.edu.shopping.dto;
 
 // 실어나르는 값
 public class OrderDTO {
-	private String receiver_name;
-	private int receiver_phonenum;
-	private String order_id;
-	private String cart_id;
-	private int final_price;
-	private int zipcode;
-	private String address;				// 주소
-	private String address_detail;		// 상세 주소
+	private String 	receiver_name;		//
+	private int 	receiver_phonenum;	//
+	private String 	order_id;			//	
+	private String 	cart_id;			//
+	private int 	final_price;		//
+	private int 	zipcode;			//
+	private String 	address;			// 주소
+	private String 	address_detail;		// 상세 주소
+	
 	public String getReceiver_name() {
 		return receiver_name;
 	}
