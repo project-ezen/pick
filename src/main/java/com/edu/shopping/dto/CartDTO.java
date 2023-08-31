55982253b2f3bc70569c733df43094ddd8072b07
DB <emfprhs1579@gmail.com>
2023-08-31 23:55:29 +0900

    modified DTO

--- src/main/java/com/edu/shopping/dto/CartDTO.java
+++ src/main/java/com/edu/shopping/dto/CartDTO.java
@@ -4,6 +4,8 @@ package com.edu.shopping.dto;
 public class CartDTO {
 	private String cart_id;
 	private String member_id;
+	private String product_id;
+	private int cart_number;
 	
 	public String getCart_id() {
 		return cart_id;
@@ -17,8 +19,21 @@ public class CartDTO {
 	public void setMember_id(String member_id) {
 		this.member_id = member_id;
 	}
+	public String getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(String product_id) {
+		this.product_id = product_id;
+	}
+	public int getCart_number() {
+		return cart_number;
+	}
+	public void setCart_number(int cart_number) {
+		this.cart_number = cart_number;
+	}
 	@Override
 	public String toString() {
-		return "CartDTO [cart_id=" + cart_id + ", member_id=" + member_id + "]";
+		return "CartDTO [cart_id=" + cart_id + ", member_id=" + member_id + ", product_id=" + product_id
+				+ ", cart_number=" + cart_number + "]";
 	}
 }
--- src/main/java/com/edu/store/dto/ProductDTO.java
+++ src/main/java/com/edu/store/dto/ProductDTO.java
@@ -4,8 +4,10 @@ package com.edu.store.dto;
 public class ProductDTO {
 	private String product_id;
 	private String product_display_id;
-	private String cart_id;
-	private String order_id;
+	private String name;
+	private String product_image;
+	private int product_price;
+	private int category_code;
 	private int count;
 	public String getProduct_id() {
 		return product_id;
@@ -19,17 +21,29 @@ public class ProductDTO {
 	public void setProduct_display_id(String product_display_id) {
 		this.product_display_id = product_display_id;
 	}
-	public String getCart_id() {
-		return cart_id;
+	public String getName() {
+		return name;
 	}
-	public void setCart_id(String cart_id) {
-		this.cart_id = cart_id;
+	public void setName(String name) {
+		this.name = name;
 	}
-	public String getOrder_id() {
-		return order_id;
+	public String getProduct_image() {
+		return product_image;
 	}
-	public void setOrder_id(String order_id) {
-		this.order_id = order_id;
+	public void setProduct_image(String product_image) {
+		this.product_image = product_image;
+	}
+	public int getProduct_price() {
+		return product_price;
+	}
+	public void setProduct_price(int product_price) {
+		this.product_price = product_price;
+	}
+	public int getCategory_code() {
+		return category_code;
+	}
+	public void setCategory_code(int category_code) {
+		this.category_code = category_code;
 	}
 	public int getCount() {
 		return count;
@@ -39,7 +53,8 @@ public class ProductDTO {
 	}
 	@Override
 	public String toString() {
-		return "ProductDTO [product_id=" + product_id + ", product_display_id=" + product_display_id + ", cart_id="
-				+ cart_id + ", order_id=" + order_id + ", count=" + count + "]";
+		return "ProductDTO [product_id=" + product_id + ", product_display_id=" + product_display_id + ", name=" + name
+				+ ", product_image=" + product_image + ", product_price=" + product_price + ", category_code="
+				+ category_code + ", count=" + count + "]";
 	}
 }
