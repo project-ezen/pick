e2998156f48fd00a3baf9428629da38495627918
juno <rnjswnsdh95@naver.com>
2023-09-04 09:42:31 +0900

    first commit

--- src/main/java/com/edu/sole/service/SoleService.java
+++ src/main/java/com/edu/sole/service/SoleService.java
@@ -1,12 +1,15 @@
 package com.edu.sole.service;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
 import com.edu.sole.dao.SoleDAO;
 import com.edu.sole.dto.SoleSearchCriteria;
 import com.edu.sole.dto.recipe.RecipeDTO;
+import com.edu.sole.dto.recipe.RecipeReviewDTO;
+import com.edu.sole.dto.recipe.ReviewCriteria;
 import com.edu.sole.dto.LiveSoleDTO;
 import com.edu.sole.dto.RecipedSoleDTO;
 
@@ -17,12 +20,19 @@ public interface SoleService {
 	
 	//술 목록의 총 개수
 	public int solecount(SoleSearchCriteria solesearchcriteria);
-	
-	
+		
 	// 술 상세한 레시피 가져오는
 	public List<RecipeDTO> solerecipeDetail(String recipe_code) throws Exception;
 
 	// 술 정보를 가져오는
 	public RecipedSoleDTO solerecipe(String recipe_code) throws Exception;
+
+	// 리뷰 Insert
+	public void soleReviewInsert(Map articleMap) throws Exception;
 	
+	//리뷰 정보 끌어오는
+	public List<RecipeReviewDTO> selectReview(ReviewCriteria reviewcri) throws Exception;
+	
+	// 리뷰 개수
+	public int reviewcount(ReviewCriteria reviewcri) throws Exception;
 }
